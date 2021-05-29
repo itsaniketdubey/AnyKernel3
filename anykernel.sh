28d3f355a410f2260be1b1ad2d0c1f4312221929
Aniket Dubey <63295358+itsaniketdubey@users.noreply.github.com>
2021-05-29 12:23:21 +0530

    Updated AnyKernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SilverCore Kernel by REIGNZ3 & Saikiran008
+kernel.string=BlazeD Kernel by HadeS
 do.devicecheck=1
 do.modules=0
 do.systemless=1
